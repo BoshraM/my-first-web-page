4979a544b3aff3ee0627d535ef52f720bafb1f2a
BoshraM <113248018+BoshraM@users.noreply.github.com>
2022-09-24 13:21:39 +0100

    dice game

--- script.js
+++ script.js
@@ -86,3 +86,97 @@ document.getElementById('button').addEventListener('click', (event) => {
   element_list.appendChild(new_li);
 
 });
+var roll, dice, total_dice, item;
+
+function randomInt(n) {
+  // Return a random number from in [0, n[
+  return Math.floor(Math.random()*n);
+}
+
+function randomMember(arr) {
+  // Return a random member of the array
+  return arr[randomInt(arr.length)]
+}
+
+// Describe this function...
+function rolls() {
+  if(--window.LoopTrap <= 0) throw "Infinite loop.";
+  let element_list2 = document.getElementById('list');
+  roll.forEach((item) => {
+    let new_li2 = document.createElement('li');
+    new_li2.innerText = item;
+
+    element_list2.appendChild(new_li2);
+  });
+  let element_total2 = document.getElementById('total');
+  element_total2.innerText = roll.reduce((a,b) => a+b, 0);
+  let element_button_roll = document.getElementById('button_roll');
+  element_button_roll.style.color = '#ff9900';
+  element_button_roll.style.backgroundColor = '#66ffff';
+  let element_button_remove = document.getElementById('button_remove');
+  element_button_remove.style.color = '#660000';
+  element_button_remove.style.backgroundColor = '#ffccff';
+  let element_button_restart = document.getElementById('button_restart');
+  element_button_restart.style.color = '#666666';
+  element_button_restart.style.backgroundColor = '#66ff99';
+}
+
+// Describe this function...
+function display_the_roll() {
+  if(--window.LoopTrap <= 0) throw "Infinite loop.";
+  let element_info = document.getElementById('info');
+  total_dice = [roll.reduce((a,b) => a+b, 0)];
+  if (total_dice == 11) {
+    element_info.innerText = 'you won';
+    element_info.style.color = '#00cccc';
+  } else if (total_dice < 11) {
+    element_info.innerText = 'keep playing';
+    element_info.style.color = '#33ff33';
+  } else if (total_dice > 11) {
+    element_info.innerText = 'you lost';
+    element_info.style.color = '#cc0000';
+  }
+}
+
+
+roll = [];
+dice = [1, 2, 3, 4, 5, 6];
+let element_p1 = document.getElementById('p1');
+element_p1.style.color = '#6600cc';
+let element_p2 = document.getElementById('p2');
+element_p2.style.color = '#6600cc';
+rolls();
+
+
+document.getElementById('button_roll').addEventListener('click', (event) => {
+  roll.push(randomMember(dice));
+  let element_list = document.getElementById('list');
+  let new_li = document.createElement('li');
+  new_li.innerText = roll.slice(-1)[0];
+
+  element_list.appendChild(new_li);
+  let element_total = document.getElementById('total');
+  element_total.replaceChildren();
+  element_total.innerText = roll.reduce((a,b) => a+b, 0);
+  display_the_roll();
+
+});
+
+document.getElementById('button_restart').addEventListener('click', (event) => {
+  roll = [];
+  let element_list3 = document.getElementById('list');
+  element_list3.replaceChildren();
+  let element_total3 = document.getElementById('total');
+  element_total3.innerText = roll.reduce((a,b) => a+b, 0);
+  display_the_roll();
+
+});
+
+document.getElementById('button_remove').addEventListener('click', (event) => {
+  let element_list4 = document.getElementById('list');
+  element_list4.replaceChildren();
+  roll.pop();
+  rolls();
+  display_the_roll();
+
+});
